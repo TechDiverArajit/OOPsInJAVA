c50564ed358b43bac4419b3328c46b722c5e2971
arajitdev <arajitdebnathofficial@gmail.com>
2025-08-03 08:45:27 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+//    public static class car{
+//        int priceInUsd;
+//        String brandName;
+//
+//        public car(int priceInUsd , String brandName ){
+//            this.priceInUsd = priceInUsd;
+//            this.brandName = brandName;
+//        }
+//    }
+
+    public static class Student{
+        int studentRoll;
+        String StudentName;
+        float marks;
+
+        public Student(int studentRoll , String studentName , float marks){
+            this.studentRoll = studentRoll;
+            this.StudentName = studentName;
+            this.marks = marks;
+        }
+
+        private void printAll(){
+            System.out.println(this.StudentName);
+            System.out.println(this.studentRoll);
+            System.out.println(this.marks);
+        }
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student(9,"Kunal",74.2f);
+        Student st2 = new Student(19,"Sanvi",78.9f);
+        st1.printAll();
+        st2.printAll();
+
+    }
+}
---
+++ src/OOPs/accessModifierss.java
@@ -0,0 +1,14 @@
+package OOPs;
+
+ class car{
+    int x = 2;
+    int getX(){
+        return x;
+    }
+}
+
+public class accessModifierss {
+    public static void main(String[] args) {
+        System.out.println("ngga");
+    }
+}
